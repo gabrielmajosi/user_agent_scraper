f50e1972b13de773385243901d09e900b84e9265
sheepsushis <59206648+sheepsushis@users.noreply.github.com>
2021-09-18 18:15:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="user_agent_scraper",
-    version="0.0.7",
+    version="1.0.0",
     author="sheepsushis",
     author_email="gmajosi1@gmail.com",
     description="scrape user agents",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="http://example.com",
+    url="https://github.com/sheepsushis/user_agent_scraper",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
